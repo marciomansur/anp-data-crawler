28477e9d286b3cc2fdf312d436b0aea42551631a
marciomansur <mrabeloo@gmail.com>
2016-03-01 11:20:40 -0300

    Initiating the crawler

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('É 10/10');
+var express = require('express');
+var config  = require('config');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+// Setting port
+app.set('port', port);
+
+app.listen(app.get('port'), (err) =>{
+
+    if(err)
+        return console.log(`Error when initializing the server: ${err}`);
+
+    console.log(`Server up, port: ${port}`);
+});
