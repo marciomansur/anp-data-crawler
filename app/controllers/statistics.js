fc9b21523410a226d5592bd7a273d4e45926e0a4
marciomansur <mrabeloo@gmail.com>
2016-03-09 09:01:47 -0300

    Removing test commands on CI

--- app/controllers/statistics.js
+++ app/controllers/statistics.js
@@ -44,29 +44,6 @@ export default class Statistic {
 
   findByState(req, res){
 
-    db.models.States
-      .findAll({
-        attributes: {
-          exclude: ['weekId']
-        },
-        where: {
-          initials: req.params.initials
-        },
-        include: [
-          {
-            model: db.models.Cities
-          }
-        ]
-      })
-      .then(data => {
-        res.json({
-          success: true,
-          status: 200,
-          data: data
-        });
-      })
-      .catch(err => {
-        res.status(400).send(err);
-      });
+
   }
 }
